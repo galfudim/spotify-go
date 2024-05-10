0e36dbfb5e7b194f43499533d3142ec9908165f6
Gal Fudim <gal.fudim@gmail.com>
2024-05-10 13:44:16 -0400

    feat: API for fetching Liked Songs library

--- main.go
+++ main.go
@@ -11,11 +11,6 @@ import (
 	"strings"
 )
 
-const (
-	Host        = "localhost:8000"
-	RedirectURI = "http://" + Host + "/callback"
-)
-
 var (
 	spotifyClientId     string
 	spotifyClientSecret string
@@ -23,21 +18,22 @@ var (
 )
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	state := "29384dz8ag823fhh" // TODO change state to random 16-char string to prevent CSRF
+	// TODO change state to random 16-char string to prevent CSRF
+	state := "29384dz8ag823fhh"
 	authURL := fmt.Sprintf("%s?client_id=%s&grant_type=%s&response_type=%s&redirect_uri=%s&scope=%s&state=%s",
 		AuthorizeUserEndpoint,
 		spotifyClientId,
-		AuthorizationCodeReqQueryParam,
-		CodeReqQueryParam,
-		RedirectURI,
-		getScopeString(),
+		"authorization_code",
+		"code",
+		"http://localhost:8000/callback",
+		"user-read-private+user-read-email+user-library-read",
 		state)
 	http.Redirect(w, r, authURL, http.StatusTemporaryRedirect)
 }
 
 func handleRedirect(w http.ResponseWriter, r *http.Request) {
-	code := r.URL.Query().Get(CodeReqQueryParam)
-	state := r.URL.Query().Get(StateReqQueryParam)
+	code := r.URL.Query().Get("code")
+	state := r.URL.Query().Get("state")
 
 	if code == "" {
 		http.Error(w, "Missing auth code", http.StatusBadRequest)
@@ -48,9 +44,9 @@ func handleRedirect(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Code: %s\nState: %s", code, state)
 
 	authReqBody := url.Values{}
-	authReqBody.Set(CodeReqBodyParam, code)
-	authReqBody.Set(RedirectURIReqBodyParam, RedirectURI)
-	authReqBody.Set(GrantTypeReqBodyParam, AuthorizationCodeReqQueryParam)
+	authReqBody.Set("code", code)
+	authReqBody.Set("redirect_uri", "http://localhost:8000/callback")
+	authReqBody.Set("grant_type", "authorization_code")
 	authRedirectReq, err := http.NewRequest(http.MethodPost, GenerateTokenEndpoint, strings.NewReader(authReqBody.Encode()))
 	if err != nil {
 		log.Fatal("Failed to create authentication request", err)
@@ -59,8 +55,8 @@ func handleRedirect(w http.ResponseWriter, r *http.Request) {
 	secrets := fmt.Sprintf("%s:%s", spotifyClientId, spotifyClientSecret)
 	encodedSecrets := base64.StdEncoding.EncodeToString([]byte(secrets))
 	authorizationHeader := fmt.Sprintf("Basic %s", encodedSecrets)
-	authRedirectReq.Header.Set(ContentTypeHeader, ApplicationFormUrlEncodedContentType)
-	authRedirectReq.Header.Set(AuthorizationHeader, authorizationHeader)
+	authRedirectReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	authRedirectReq.Header.Set("Authorization", authorizationHeader)
 
 	client := http.Client{}
 	resp, err := client.Do(authRedirectReq)
@@ -71,12 +67,12 @@ func handleRedirect(w http.ResponseWriter, r *http.Request) {
 	responseBody, _ := io.ReadAll(resp.Body)
 	authToken := jsonToMap(string(responseBody))
 	bearerToken = "Bearer " + fmt.Sprintf("%v", authToken["access_token"])
-	log.Printf("Bearer token:\n%s", bearerToken)
+	//log.Printf("Bearer token:\n%s", bearerToken)
 }
 
 func handleUser(w http.ResponseWriter, r *http.Request) {
 	req, err := http.NewRequest(http.MethodGet, GetCurrentUserProfileEndpoint, nil)
-	req.Header.Set(AuthorizationHeader, bearerToken)
+	req.Header.Set("Authorization", bearerToken)
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -87,16 +83,39 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleUserTracks(w http.ResponseWriter, r *http.Request) {
-	req, err := http.NewRequest(http.MethodGet, GetCurrentUserSavedTracksEndpoint, nil)
-	req.Header.Set(AuthorizationHeader, bearerToken)
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Failed to hit \"tracks\" endpoint")
+	// arr to collect values
+	arr := []interface{}{}
+	endpoint := "https://api.spotify.com/v1/me/tracks?limit=50&offset=0"
+
+	for {
+		req, _ := http.NewRequest(http.MethodGet, endpoint, nil)
+		req.Header.Set("Authorization", bearerToken)
+		client := http.Client{}
+		resp, _ := client.Do(req)
+
+		b, _ := io.ReadAll(resp.Body)
+		tracks := jsonToMap(string(b))
+		for key, value := range tracks {
+			// TODO ad items to array
+			if key == "items" {
+				values := value.([]interface{}) // arr of tracks with added date and track data
+				for _, track := range values {
+					trackData := track.(map[string]interface{})["track"]
+					song := trackData.(map[string]interface{})
+					fmt.Fprintf(w, "song: %s\n", song["name"])
+					arr = append(arr, song)
+				}
+			} else if key == "next" {
+				if value != nil {
+					endpoint = fmt.Sprintf("%s", value.(string))
+					log.Printf("endpoint: %s", endpoint)
+				} else {
+					log.Printf("%d total items collected\n", len(arr))
+					return
+				}
+			}
+		}
 	}
-	b, _ := io.ReadAll(resp.Body)
-	tracks := jsonToMap(string(b))
-	fmt.Fprintf(w, "tracks is %s\n", tracks)
 }
 
 func main() {
@@ -108,7 +127,7 @@ func main() {
 	http.HandleFunc("/", handleUser)
 	http.HandleFunc("/tracks", handleUserTracks)
 
-	log.Printf("Server listening on: %s", Host)
-	serverError := http.ListenAndServe(Host, nil)
+	log.Println("Server listening on localhost:8000")
+	serverError := http.ListenAndServe("localhost:8000", nil)
 	log.Fatalf("Server killed: %s", serverError)
 }
--- utils.go
+++ utils.go
@@ -2,79 +2,16 @@ package main
 
 import "encoding/json"
 
-type Scope string
-
-const (
-	UserReadPrivateScope Scope = "user-read-private"
-	UserEmailReadScope   Scope = "user-read-email"
-
-	UserLibraryReadScope Scope = "user-library-read"
-)
-
 const (
 	ClientId     = "SPOTIFY_CLIENT_ID"
 	ClientSecret = "SPOTIFY_CLIENT_SECRET"
 )
 
 const (
-	SpotifyAccountEndpoint = "https://accounts.spotify.com"
-	SpotifyApiEndpoint     = "https://api.spotify.com"
-	v1ApiEndpoint          = SpotifyApiEndpoint + "/v1"
-	AuthorizeUserEndpoint  = SpotifyAccountEndpoint + "/authorize"
-	GenerateTokenEndpoint  = SpotifyAccountEndpoint + "/api/token"
+	AuthorizeUserEndpoint         = "https://accounts.spotify.com/authorize"
+	GenerateTokenEndpoint         = "https://accounts.spotify.com/api/token"
+	GetCurrentUserProfileEndpoint = "https://api.spotify.com/v1/me"
 )
-
-// Resources
-const (
-	CurrentUserEndpoint = v1ApiEndpoint + "/me"
-)
-
-// v1 APIs
-const (
-	GetCurrentUserProfileEndpoint     = CurrentUserEndpoint
-	GetCurrentUserSavedTracksEndpoint = CurrentUserEndpoint + "/tracks"
-)
-
-// HTTP request query params
-const (
-	CodeReqQueryParam              = "code"
-	StateReqQueryParam             = "state"
-	AuthorizationCodeReqQueryParam = "authorization_code"
-)
-
-// HTTP request body params
-const (
-	CodeReqBodyParam        = "code"
-	RedirectURIReqBodyParam = "redirect_uri"
-	GrantTypeReqBodyParam   = "grant_type"
-)
-
-// HTTP request headers - values
-const ApplicationFormUrlEncodedContentType = "application/x-www-form-urlencoded"
-
-// HTTP request headers
-const (
-	ContentTypeHeader   = "Content-Type"
-	AuthorizationHeader = "Authorization"
-)
-
-func getAllScopes() []Scope {
-	return []Scope{UserReadPrivateScope, UserEmailReadScope, UserLibraryReadScope}
-}
-
-func getScopeString() string {
-	allScopes := getAllScopes()
-	scopeString := ""
-	for i := 0; i < len(allScopes); i++ {
-		if i != len(allScopes)-1 {
-			scopeString += string(allScopes[i]) + "+"
-		} else {
-			scopeString += string(allScopes[i])
-		}
-	}
-
-	return scopeString
-}
 
 func jsonToMap(jsonStr string) map[string]interface{} {
 	result := make(map[string]interface{})
